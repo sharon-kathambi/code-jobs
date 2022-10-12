f1b699a158bf739adc12109d642e029c91f4d699
Sharon Mwenda <smkathambi@gmail.com>
2022-10-12 18:43:46 +0300

    added index and show for company controller

--- app/controllers/companys_controller.rb
+++ app/controllers/companys_controller.rb
@@ -1,2 +1,16 @@
 class CompanysController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+    def index
+        companys = Company.all
+        render json: companys
+    end
+
+    def show
+        company = Company.find_by(id: params[:id])
+        render json: company
+    end
+
+    def render_not_found
+        render json: {error: 'company not found'}, status: :not_found
+    end
 end
