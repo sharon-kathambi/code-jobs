3d549d6a5eaa84ab7da1814f40be27b5a5f4076b
Sharon Mwenda <smkathambi@gmail.com>
2022-10-12 17:45:52 +0300

    added tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export const purge = ["./client/src/**/*.{js,jsx,ts,tsx}", "./public/index.html", "client/src/index.css"];
+export const darkMode = false;
+export const theme = {
+  fontFamily: {
+    body: ["Spartan", "sans-serif"],
+  },
+  extend: {
+    colors: {
+      cyan: {
+        default: "hsl(180, 29%, 50%)",
+        verylight: "hsl(180, 52%, 96%)",
+        light: " hsl(180, 31%, 95%)",
+        dark: "hsl(180, 8%, 52%)",
+        verydark: "hsl(180, 14%, 20%)",
+      },
+    },
+    boxShadow: {
+      cyan: "0px 10px 20px 0 hsl(180deg 29% 50% / 25%)",
+    },
+    fontSize: {
+      base: "15px",
+    },
+  },
+};
+export const variants = {
+  extend: {},
+};
+export const plugins = [];
