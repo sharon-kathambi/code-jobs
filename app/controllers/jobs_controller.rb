12f9a577e90e3d0f7c1e0ecaed1a0a1e4f8d8af2
Sharon Mwenda <smkathambi@gmail.com>
2022-10-12 18:35:28 +0300

    added create,destroy update in job controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,5 @@
 class JobsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
     def index 
         jobs = Job.all
         render json: jobs
@@ -6,10 +7,33 @@ class JobsController < ApplicationController
 
     def show
         job = Job.find_by(id: params[:id])
-        if job
-            render json: job
-        else
-            render json: {error: 'job not found'}, status: :not_found
-        end
+        render json: job
+    end
+
+    def create
+        job = Job.create(job_params)
+        render json: job, status: :created
+
+    end
+
+    def update
+        job = Job.find_by(id: params[:id])
+        job.update(job_params)
+        render json: job, status: :accepted
+    end
+
+    def destroy
+        job = Job.find_by(id: params[:id])
+        job.destroy
+        head :no_content
+    end
+
+    private
+    def render_not_found
+        render json: {error: 'job not found'}, status: :not_found
+    end
+
+    def job_params
+        params.permit(:name, :location, :level :salary, :languages, :contract)
     end
 end
